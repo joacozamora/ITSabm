09a29847a3bb882440a007488e6371df4943cc38
Joaquin <joacozamoracab@gmail.com>
2023-10-30 20:02:40 -0300

    Nuevo commit

---
+++ Entidades/Alumno.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Alumno
+    {
+        #region Atributos
+        private int DNI;
+        private string NombreApellido;
+        private string Fecha_Nac;
+        private string Email;
+        private bool Analitico;
+        #endregion
+
+        #region Constructores
+        public Alumno()
+        { }
+        public Alumno(int Dni, string nomApellido, string fecha_Nac, string email, bool analitico)
+        {
+            DNI = Dni;
+            NombreApellido = nomApellido;
+            Fecha_Nac = fecha_Nac;
+            Email = email;
+            Analitico = analitico;
+        }
+        #endregion
+
+        #region Metodos
+        
+        #endregion
+    }
+
+
+}
---
+++ Entidades/Materia.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class Materia
+    {
+        #region Atributos
+        private int Codigo;
+        private string Nombre;
+        private int Año_Cursado;
+        private string Dia_Cursado;
+        private string Nombre_Carrera;
+        #endregion
+
+        #region Constructores
+        public Materia()
+        { }
+        public Materia(int Cod, string Nom, int Año, string Dia_Clases, string Carrera)
+        {
+            Codigo = Cod;
+            Nombre = Nom;
+            Año_Cursado = Año;
+            Dia_Cursado = Dia_Clases;
+            Nombre_Carrera = Carrera;
+
+        }
+        #endregion
+    }
+}
